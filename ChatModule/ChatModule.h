cd5e485b1dd034d37fa7ea20ce95e06d0a53a2a2
taronveq <taron.veqilyan@gmail.com>
2019-11-19 13:25:01 +0400

    Initial commit

---
+++ ChatModule/ChatModule.h
@@ -0,0 +1,19 @@
+//
+//  ChatModule.h
+//  ChatModule
+//
+//  Created by iMac on 11/19/19.
+//  Copyright © 2019 iMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatModule.
+FOUNDATION_EXPORT double ChatModuleVersionNumber;
+
+//! Project version string for ChatModule.
+FOUNDATION_EXPORT const unsigned char ChatModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatModule/PublicHeader.h>
+
+
